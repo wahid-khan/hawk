299d97332d89139c047006673740eaf18db697d2
DESKTOP-AJHNIV1\WAHID <h.a.wahid.khan1997@gmail.com>
2020-12-06 14:42:02 +0500

    practice

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -27,11 +27,63 @@ class DashboardController extends Controller
     {
         $user_id =auth()->user()->id;
         $user = User::find($user_id);
+        
+        // DB Connecttion require
+        //$users = DB::connection('mysql')->select('SELECT * FROM users Where id' . '=' . $user_id);
+        
+
+        /*
+        #Retrieving All Rows From A Table
+        */
+        // $users = DB::table('users')->get();
+        // dd($users);
+
+        /*
+        #Retrieving A Single Row
+        */
+        // $users = DB::table('users')->first();
+        // dd($users);
+        
+        /*
+        #Retrieving A Single Row / Column From A Table
+        */
+        // $users = DB::table('users')
+        // ->where('name', 'wahid')->first();
+        // dd($users);
+
+        /*
+        #Retrieving A Single Value Column From A Table
+        */
+        // $users = DB::table('users')
+        // ->where('name', 'wahid')->value('name');
+        // dd($users);
+
+        /*
+        #etrieve a single row by its id
+        */
+        // $users = DB::table('users')->find(1);
+        // dd($users);
+        
+        /*
+        #Retrieve a single row by its id column value & u can call individual columns
+        */
+        // $users = DB::table('users')->find(1);
+        // $name = $users->name;
+        // dd($users->name, $users->email);
+
+        /*
+        #Retrieving A List Of Column Values
+        */
+        // $posts = DB::table('posts')->pluck('user_id', 'id'); //here 'id' is customer key column
+        // $users = DB::table('users')->pluck('name', 'id');
+        // dd($posts, $users);
+
         // $users = DB::table('users')->paginate(15);
         //return auth()->user()->id;
         //return view('/dashboard')->with('users' , $user->posts);
         return view('/dashboard')->with('posts', $user->posts()->paginate(5));
         //return auth()->user()->id;
+        
         
     }
 }
